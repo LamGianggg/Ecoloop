ee8ea7ca158056a2e6d37fd9289a0ced3b63a0ed
LamGianggg <lgzero4350@gmail.com>
2026-04-12 22:24:51 +0700

    update

--- app.js
+++ app.js
@@ -166,3 +166,10 @@ document.addEventListener('DOMContentLoaded', function () {
     // INIT
     resetAll();
 });
+
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("mobile-menu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
